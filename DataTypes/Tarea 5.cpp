635682fd76740c7b6d2da7f64676c9c9bf9e6b24
JPachon04 <160759904+JPachon04@users.noreply.github.com>
2024-02-23 17:39:21 -0500

    Add files via upload

---
+++ DataTypes/Tarea 5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {//Uno puede usar valores ASCII para mostrar ciertos valores
+	char f=70 , g=71 , h=72;//En este caso 70, 71 72 representan las letras FGH de acuerdo a la lista ASCII
+	cout << f;
+	cout << g;
+	cout << h;
+	return 0;
+}
