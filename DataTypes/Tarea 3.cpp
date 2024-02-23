acd301b44d0ac26dbf6bf7fb94cfebfbf406c77f
JPachon04 <160759904+JPachon04@users.noreply.github.com>
2024-02-23 17:01:10 -0500

    Add files via upload

---
+++ DataTypes/Tarea 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	bool isCodingFun = true;
+	bool isFishTasty = false;
+	cout << isCodingFun<<"\20"; // Este es el numero verdadero 1 (true)
+	cout << isFishTasty;  // Outputs 0 (false)
+	return 0;
+}
