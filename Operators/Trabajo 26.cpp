57e7dd6e524e1df57dc5c814b175ef7fc5653fb0
JPachon04 <160759904+JPachon04@users.noreply.github.com>
2024-02-23 21:45:20 -0500

    Add files via upload

---
+++ Operators/Trabajo 26.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int x=6;
+	int y=7;
+	cout<<(x<7||x>9);//Da 1 por que una de las igualdades es verdadera
+	return 0;
+}
