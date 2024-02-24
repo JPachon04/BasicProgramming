7c812995cd99f7ae6493629a0a267f952624fd1e
JPachon04 <160759904+JPachon04@users.noreply.github.com>
2024-02-23 21:28:45 -0500

    Add files via upload

---
+++ Operators/Trabajo 23.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int x=10;
+	int y=10;
+	cout<<(x>=y);//Da 1 por que es verdad
+	return 0;
+}
