8c0763615b81547864e8eef1b48c4436108867d0
JPachon04 <160759904+JPachon04@users.noreply.github.com>
2024-03-06 10:49:26 -0500

    Add files via upload

---
+++ Conditionals/Ejercico 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int day=4;
+	switch(day){
+		case 6:
+			cout<<"Hoy es sabado";
+			break;
+		case 7:
+			cout<<"Hor es domingo";
+			break;
+		default:
+		cout<<"Buscando el dia;(";
+	}
+	
+	return 0;
+}
