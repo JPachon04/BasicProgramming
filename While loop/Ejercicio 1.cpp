d0975a0fecebfd2040674700a915fc7e78ebf27f
JPachon04 <160759904+JPachon04@users.noreply.github.com>
2024-03-13 10:41:31 -0500

    Add files via upload

---
+++ While loop/Ejercicio 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int a=10;
+	while (a<20){
+	cout<<"Valor de a:"<<a<<endl;
+	a++;
+	}
+	return 0;
+}
